781013c6556bd1ec310221cfdb5dc58247c93d7b
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-01 14:18:20 +0700

    feat: added types file in project

---
+++ additional.d.ts
@@ -0,0 +1,38 @@
+// content assets
+
+declare module '*.svg' {
+	import React = require('react');
+	export const ReactComponent: React.FunctionComponent<
+		React.SVGProps<SVGSVGElement>
+	>;
+	const src: string;
+	export default src;
+}
+
+declare module '*.png';
+
+declare module '*.jpg';
+
+declare module '*.json';
+
+declare module '*.pdf' {
+	const value: string;
+	export default value;
+}
+
+// scss/saas/css
+
+declare module '*.module.css' {
+	const classes: { [key: string]: string };
+	export default classes;
+}
+
+declare module '*.module.scss' {
+	const classes: { [key: string]: string };
+	export default classes;
+}
+
+declare module '*.module.sass' {
+	const classes: { [key: string]: string };
+	export default classes;
+}
