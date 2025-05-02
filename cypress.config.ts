8c1aa4f4b709ada213ae2ee5ca1e03a0570bb6b6
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-03 05:22:43 +0700

    fix: remove old url in cypress.config for web-server e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 		setupNodeEvents(on, config) {
 			return config;
 		},
-		baseUrl: process.env.CYPRESS_baseUrl || 'http://localhost:3000',
+		baseUrl: process.env.CYPRESS_baseUrl || 'http://localhost:8081',
 		specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
 		supportFile: 'cypress/support/e2e.ts'
 	}
