f8fba992ce60382706417837bd07e3ae3ca4b04b
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-02 19:23:51 +0700

    refactor: refact types in TextDecoder

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,4 +3,9 @@ import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder as any;
+global.TextDecoder = TextDecoder as typeof globalThis extends {
+	onmessage: any;
+	TextDecoder: infer TextDecoder;
+}
+	? TextDecoder
+	: typeof TextDecoder;
