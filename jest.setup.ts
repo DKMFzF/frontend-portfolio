6bce5f97778ce10ee9153eb2f88368c503997a1a
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-02 23:46:47 +0700

    docs: added docs in jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 
 import { TextEncoder, TextDecoder } from 'util';
 
+// добавляем API TextEncoder и TextDecoder в тестовую среду jest
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder as typeof globalThis extends {
 	onmessage: any;
