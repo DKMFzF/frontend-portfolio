3a78926d77c91ca27a076d151bd045139de67946
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-04 00:51:25 +0700

    refactor: added constats values yandex and google anaysis settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { createRoot } from 'react-dom/client';
-import { StrictMode, Suspense } from 'react';
+import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import 'core-js/stable';
 import { router } from './routes';
 import { Analytic } from './context/analityc';
 import store from './services/store';
-import 'core-js/stable';
+import { YANDEX_METRIC, GOOOGLE_ANALYSIS } from '@utils-constants';
 import './styles/index.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
@@ -16,8 +17,8 @@ root.render(
 	<StrictMode>
 		<Analytic.Provider
 			value={{
-				yandexId: 101530999,
-				googleId: 'G-QC3J4L6VP4'
+				yandexId: YANDEX_METRIC,
+				googleId: GOOOGLE_ANALYSIS
 			}}
 		>
 			<Provider store={store}>
