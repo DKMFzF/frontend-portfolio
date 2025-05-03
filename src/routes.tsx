d1e71c49d7e74a6eeb8dd4d2c02edf7191114dbd
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-03 19:12:53 +0700

    fix: added basename in router

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,26 +2,29 @@ import { createBrowserRouter } from 'react-router-dom';
 import { AppLayout } from './components';
 import { AboutMePage, NotFound404, PortfolioPage } from './pages';
 
-const basename =
-	process.env.NODE_ENV === 'production' ? '/frontend-portfolio' : '/';
-
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <AppLayout />,
+			children: [
+				{
+					index: true,
+					element: <AboutMePage />
+				},
+				{
+					path: 'portfolio',
+					element: <PortfolioPage />
+				},
+				{
+					path: '*',
+					element: <NotFound404 />
+				}
+			]
+		}
+	],
 	{
-		path: basename,
-		element: <AppLayout />,
-		children: [
-			{
-				index: true,
-				element: <AboutMePage />
-			},
-			{
-				path: `${basename}/portfolio`,
-				element: <PortfolioPage />
-			},
-			{
-				path: '*',
-				element: <NotFound404 />
-			}
-		]
+		basename:
+			process.env.NODE_ENV === 'production' ? '/frontend-portfolio' : '/'
 	}
-]);
+);
