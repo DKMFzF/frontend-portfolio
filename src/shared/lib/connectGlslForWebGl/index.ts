cc4e84960ac169fcba3b2fbd526b220887189c10
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-16 00:13:35 +0700

    feat(three): added new page sketches-page and new hook connectglslforwebgl

---
+++ src/shared/lib/connectGlslForWebGl/index.ts
@@ -0,0 +1,1 @@
+export { useShaderRenderer } from './useConnectGlslForWebGl';
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './cookie';
 export * from './seoMetrika';
 export * from './animatedNavigate';
+export * from './connectGlslForWebGl';
