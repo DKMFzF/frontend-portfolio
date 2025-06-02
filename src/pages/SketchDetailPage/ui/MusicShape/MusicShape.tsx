c75652a984223df7912bdeb8769cccb8a747635d
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-06-02 10:12:48 +0700

    feat: replace music

--- src/pages/SketchDetailPage/ui/MusicShape/MusicShape.tsx
+++ src/pages/SketchDetailPage/ui/MusicShape/MusicShape.tsx
@@ -1,11 +1,11 @@
 import { FC, Suspense } from 'react';
 import { MusicSphere } from './MusicSphere';
-import music from './lib/music.mp3';
+import music from './lib/Zachz Winner - lover online _ Pluggnb _ NCS - Copyright Free Music.mp3';
 import { Preloader } from '@ui';
 
 export const MusicShape: FC = () => (
 	<Suspense fallback={<Preloader />}>
-		<MusicSphere audioUrl={music} sensitivity={0.7} />
+		<MusicSphere audioUrl={music} sensitivity={1} />
 	</Suspense>
 );
 
